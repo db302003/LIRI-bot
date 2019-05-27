9273af5b5b784736b8bb731ffb9d3bb06d1a9f8e
Dawn Benton <46697755+db302003@users.noreply.github.com>
2019-05-27 18:38:40 -0500

    Add files via upload

---
+++ liri.js
@@ -0,0 +1,140 @@
+
+let axios = require("axios");
+let Spotify = require('node-spotify-api');
+let dotenv = require('moment');
+let fs = require('fs');
+
+//let keys = require('key.js');
+//let spotify2 = new Spotify(keys.spotify);
+
+var moment = require('moment');
+moment().format();
+
+
+//Install npm & initialize package.json
+
+let command = process.argv[2];
+let input = process.argv[3];
+
+
+let spotify = new Spotify({
+    id: '92e5efbb11c74d41891e78f018964161',
+    secret: 'd2cc071cda2a49c693987757e7011925',
+});
+
+
+// 4 commands 
+// concert-this
+// spotify-this-song
+// movie-this
+// do-what-it-says
+
+
+switch(command){
+        case 'concert-this':
+        let concertURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+        axios.get(concertURL).then(function(response){
+            let data = response.data[0];
+
+            let concertData = [
+                "---------------------",
+                "Artist: " + input,
+                "\nVenue: " + data.venue.name,
+                "\nLocation: " + data.venue.city + ", " + data.venue.region,
+                "\nDate: " +  moment(data.datetime).format("MM/DD/YYYY"),
+                "----------------------",
+            ].join("\n");
+
+            console.log(concertData);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+
+    console.log("concert command test");
+
+     break;
+
+
+    case 'spotify-this-song':
+        spotify.search({ type: "track" , query: input})
+            .then(function(response){
+            let data = response.tracks.items[0].album;
+
+            let songData = [
+                "---------------------------",
+                "Artist: " + data.artists[0].name,
+                "\nSong: " + input,
+                "\nSpotify Link: " + data.artists[0].external_urls.spotify,
+                "n\Album: " + data.name,
+                "----------------------------"
+            ].join("\n\n");
+
+            console.log(songData);
+
+            })
+        .catch(function(err){
+            console.log(err);
+        });
+
+    break;
+
+
+    case 'movie-this':
+    console.log("///////// Movie Info Below /////////");
+    axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy")
+    .then(function(response){
+        console.log(response.data.Title);
+        console.log(response.data.Year);
+        console.log(response.data.Rated);
+        console.log(response.data.Country);
+        console.log(response.data.Language);
+        console.log(response.data.Plot);
+        console.log(response.data.Actors)
+    });
+
+    
+    console.log("movie command test");
+    break;
+
+
+    case 'do-what-it-says':
+        fs.readFile("random.txt", "UTF-8", function(error,data){
+            if (error){
+                console.log("Error; " + error);
+
+            } else {
+                let choice = data.split(",");
+
+                console.log(choice[1]);
+
+                // run anything through spotify
+                spotify.search({ type: "track" , query: choice[1] })
+                .then(function(response){
+                let data = response.tracks.items[0].album;
+    
+                let songData = [
+                    "---------------------------",
+                    "Artist: " + data.artists[0].name,
+                    "\nSong: " + choice[1],
+                    "\nSpotify Link: " + data.artists[0].external_urls.spotify,
+                    "n\Album: " + data.name,
+                    "----------------------------"
+                ].join("\n\n");
+    
+                console.log(songData);
+    
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+            }
+        });
+
+    console.log("filesystem command test");
+    break;
+
+    default:
+    console.log("Invalid Command");
+
+}
